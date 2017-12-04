1d5ae2f9ee97658c2ed3d56f72ca5b36fd12f27a
pablohc <pablonoviello@outlook.com>
2017-12-03 23:59:21 -0300

    prueba de cambios

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 <?php require '../private/Functions/initialize.php';?>
 <?php include('shared/public_header.php'); ?>
 <?php include('shared/public_navigation.php'); ?>
-
+<!-- Pablo -->
 <?php $promociones = promociones(); ?>
 <?php $ruta = '/EducAr/public/img/promo/'; ?>
 
