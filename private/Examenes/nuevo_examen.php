9f5224d2ff1636fd012f4ca977db3b596e3e933a
pablohc <pablonoviello@outlook.com>
2017-12-06 04:41:35 -0300

    FALTA FILTRAR QUE CUANDO SE ELIJA UNA MATERIA, NOS TRAIGA SOLO LOS TEMAS DE ESA MATERIA

--- private/Examenes/nuevo_examen.php
+++ private/Examenes/nuevo_examen.php
@@ -7,7 +7,8 @@ if ($_SESSION['permisos']!="ADMIN") {header('location:'.url_for('private/index.p
 <?php include(SHARED_PATH.'/private_header.php'); ?>
 <?php include(SHARED_PATH.'/private_navigation.php'); ?>
 <?php include(SHARED_PATH.'/private_sidebar.php'); ?>
-
+<?php $materias = materias(); ?>
+<?php $temas = temas(); ?>
 
                 <!-- Contact Form -->
                 <!-- In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
@@ -15,30 +16,31 @@ if ($_SESSION['permisos']!="ADMIN") {header('location:'.url_for('private/index.p
                     <div class="col-md-8">
                         <br>
                         <form name="sentMessage" id="contactForm" action="../Examenes/crear_examen.php" method="post" >
-                          <div class="control-group form-group">
-                              <div class="controls">
-                                  <label>Materia:</label>
-                                  <select name="mat_nombre" class="form-control">
-                                    <option value=""></option>
-                                    <option value="mat_id">TEMA 1</option>
-                                    <option value="mat_id">TEMA 2</option>
-                                    <option value="mat_id">TEMA 3</option>
-                                  </select>
-                              </div>
-                          </div>
-                          <hr><br>
-                          <div class="control-group form-group">
-                              <div class="controls">
-                                  <label>Tema:</label>
-                                  <select name="tema_nombre" class="form-control">
-                                    <option value=""></option>
-                                    <option value="tema_id">TEMA 1</option>
-                                    <option value="tema_id">TEMA 2</option>
-                                    <option value="tema_id">TEMA 3</option>
-                                  </select>
-                              </div>
-                          </div>
-                          <hr><br>
+						  <div>
+							<label>Materia:</label>
+							<select name='mat_id' class="form-control">
+								<option value="">Seleccionar</option>
+								<?php while($materia = mysqli_fetch_assoc($materias)){ ?>
+								<option value="<?php echo h($tema['mat_id']); ?>"><?php echo h($materia['mat_nombre']); ?></option>
+								<?php }; ?>
+							</select>
+						  </div>
+
+							<br>
+							FALTA FILTRAR QUE CUANDO SE ELIJA UNA MATERIA, NOS TRAIGA SOLO LOS TEMAS DE ESA MATERIA
+						  <div>
+							<label>Tema:</label>
+							<select name='tema_id' class="form-control">
+								<option value="">Seleccionar</option>
+								<?php while($tema = mysqli_fetch_assoc($temas)){ ?>
+								<option value="<?php echo h($tema['tema_id']); ?>"><?php echo h($tema['tema_nombre']); ?></option>
+								<?php }; ?>
+							</select>
+						  </div>
+							
+						  <br>
+                          <hr>
+						  <br>
                           <?php
                           $preg = 1;
                           while ($preg<3) {
@@ -47,7 +49,7 @@ if ($_SESSION['permisos']!="ADMIN") {header('location:'.url_for('private/index.p
                                 <div class='controls'>
                                     <h5><label>Pregunta ".$preg.":</label>
                                     <input type='text' class='form-control' name= 'Pregunta-".$preg."' 'required data-validation-required-message= Ingrese Pregunta ".$preg."></h5>
-                                    <p class='help-block'></p>
+                                    <p class='help-block'></p><br>
                                 </div>
                             </div>";
                             $opc=1;
@@ -64,7 +66,7 @@ if ($_SESSION['permisos']!="ADMIN") {header('location:'.url_for('private/index.p
                                 $opc=$opc+1;
                               }
                             $preg=$preg+1;
-                            echo "<hr><br>"; ;
+                            echo "<br><hr><br>"; ;
                             }
                              ?>
                             <div id="success"></div>
