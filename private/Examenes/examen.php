25ae0febe743bc852bc5599fe36d8e0d22740d71
GuillermoRosello <guillerosello@gmail.com>
2017-12-06 20:28:10 -0300

    examen standalone

--- private/Examenes/examen.php
+++ private/Examenes/examen.php
@@ -1,3 +1,28 @@
 <?php $page_title ='Examen'; ?>
-
-<?php require_once ('../Functions/generar_examen.php'); ?>
+<?php require_once ('../Functions/initialize.php');?>
+<?php include(SHARED_PATH.'/private_header.php'); ?>
+<?php /*$tema=$_POST['tema'];*/
+$tema=0; ?>
+<script type="text/javascript">
+  setTimeout(function() {
+  document.getElementById("submit").click();
+}, 10000);
+</script>
+<?php include(SHARED_PATH.'/private_navigation.php'); ?>
+  <div class="container">
+    <div class="col-lg-12">
+      <div class="jumbotron">
+          <h4><span class="Examen">Examen</span></h4> <br>
+          <p>Tiene 10 min para completar el examen</p>
+          <button onClick="window.print()" class="btn btn-primary" >Imprimir</button>
+          <form action="resultados.php" method="post">
+          <ul>
+          <?php preguntas_examen(0);?>
+          </ul>
+      <p><input type=submit name=examen class="btn btn-primary" value=Evaluar /></p>
+          <input type=submit id="submit" name=examen class="btn btn-primary" value=Evaluar formnovalidate style="display: none;">
+      </form>
+    </div>
+  </div>
+<?php include(SHARED_PATH.'/private_footer.php'); ?>
+</div>
